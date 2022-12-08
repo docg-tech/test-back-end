84f5f981f2ce960d3c8ed1c94cb89b07ede7bd59
Victor Paim <victorspaim@hotmail.com>
2022-12-08 11:40:13 -0300

    feat: Adiciona Alembic e SQLAlchemy

--- test_back_end/main.py
+++ test_back_end/main.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 app = Flask(__name__)
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "mysql://paim:159632@localhost:3306/test_back_end"
+db.init_app(app)
 
 
 @app.route("/")
