c2c031c34d81119e214b8f9b58e5767ce9b3c6d2
Victor Paim <victorspaim@hotmail.com>
2022-12-12 14:43:45 -0300

    feat: Cria coluna titulo na tabela Servico

--- test_back_end/models/servico.py
+++ test_back_end/models/servico.py
@@ -6,4 +6,5 @@ from . import db
 class Servico(db.Model):
     __tablename__ = "servico"
     id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(255), nullable=False)
     preco = db.Column(db.Numeric(precision=8, scale=2))
