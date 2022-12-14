4b1abafcfc527b472269c0f809346bfb1d4713c0
Victor Paim <victorspaim@hotmail.com>
2022-12-14 11:52:02 -0300

    feat(servico): Altera formato para data agendamento para dia/mes/ano

--- test_back_end/schemas/servico.py
+++ test_back_end/schemas/servico.py
@@ -5,5 +5,5 @@ class ServicoSchema(Schema):
     id = fields.Integer()
     titulo = fields.String()
     preco = fields.Decimal(places=2, as_string=True)
-    data_agendamento = fields.DateTime(format="iso")
+    data_agendamento = fields.DateTime(format="%d/%m/%Y")
     pet_id = fields.Integer()
